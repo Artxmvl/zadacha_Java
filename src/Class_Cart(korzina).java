01974feab1f86cb86867c5e17e688d18ebc916d6
Artxmvl <talakhovartem2007@gmail.com>
2026-01-11 04:22:45 +0300

    Update Class_Cart(korzina).java

--- src/Class_Cart(korzina).java
+++ src/Class_Cart(korzina).java
@@ -1,31 +1,41 @@
 import java.util.ArrayList;
 
+// Класс корзины: хранит список товаров и в то же время с ним работает
 public class Cart {
+    // Список товаров в корзине
     private ArrayList<Product> products = new ArrayList<>();
 
+    // Добавить товар в корзину
     public void addProduct(Product p) {
         products.add(p);
     }
 
+    // Удалить  товар по названию
     public void removeProduct(String name) {
-        products.removeIf(p -> p.getName().equalsIgnoreCase(name));
-    }
-
-    public double getTotal() {
-        double sum = 0;
-        for (Product p : products) {
-            sum += p.getPrice();
+        for (int i = 0; i < products.size(); i++) {
+            if (products.get(i).getName().equals(name)) {
+                products.remove(i);  // Удаляем и выходим из метода
+                return;
+            }
         }
-        return sum;
     }
 
+    // Посчитать общую сумму всего
+    public double getTotal() {
+        double total = 0.0;
+        for (Product p : products) {
+            total += p.getPrice();
+        }
+        return total;
+    }
+
+    // Вывести содержимое корзины в консоль
     public void printCart() {
         System.out.println("=== Корзина ===");
-        int i = 1;
-        for (Product p : products) {
-            System.out.println(i + ". " + p);
-            i++;
+        for (int i = 0; i < products.size(); i++) {
+            // Нумеруем товары с 1
+            System.out.println((i + 1) + ". " + products.get(i).toString());
         }
-        System.out.println("Итого: " + getTotal() + " руб.\n");
+        System.out.println("Итого: " + getTotal() + " руб.");
     }
 }
